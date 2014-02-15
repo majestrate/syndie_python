2c96593ce6370405145a06c8cfc4d8275b292fc7
Architect <fcb5984c@opayq.com>
2014-02-15 06:28:20 -0500

    Improve setup.py
    Packaging, dependencies, and meta specifications.

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3.3
+
+''' Handles packaging and setup '''
+
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
-    name='syndie',
-    version='0.0.1',
-    description='Python implementation of the high latency network agnostic distributed forum syndie',
-    author='Jeff',
-    author_email='ampernand@gmail.com',
-    packages=['syndie'],
-    package_dir={'syndie':'src'}
+
+	# Basic package information.
+    name = 'syndie',
+    version = '0.0.1',
+    packages = ['syndie'],
+
+    # Packaging options.
+    zip_safe = False,
+    include_package_data = True,
+
+    # Package dependencies to be added later.
+    install_requires = [''],
+    tests_require = [''],
+
+    # Metadata for PyPI.
+    author = 'Jeff',
+    author_email = 'ampernand@gmail.com',
+    license = '',
+    url = '',
+    keywords = '',
+    description = 'Python implementation of the high latency network agnostic' \
+    	' distributed forum syndie.',
+    package_dir= {'syndie':'src'}
+
     )
